5f9e519e0bfafae659b7f544913df5846ca9c5d2
ebeatty77 <139764201+ebeatty77@users.noreply.github.com>
2025-04-07 16:16:11 -0400

    Update nicegui.py

--- nicegui.py
+++ nicegui.py
@@ -1,4 +1,15 @@
-pip3 install nicegui
+from http.server import BaseHTTPRequestHandler
 from nicegui import ui
-ui.label('Hello NiceGUI!')
-ui.run()
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"<html><body>")
+        self.wfile.write(b"<h1>Hello, NiceGUI on Vercel!</h1>")
+        self.wfile.write(b"</body></html>")
+        return
+
+if __name__ == "__main__":
+    ui.run(host="0.0.0.0", port=8080)
